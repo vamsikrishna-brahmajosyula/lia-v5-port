78eaee9e320c704d7e20a6162871c3a9d7c48d7c
Vamsi Krishna Brahmajosyula <vamsikrishna.brahmajosyula@gmail.com>
2015-06-16 21:33:18 +0530

    Whitespace Analyser is now in .core package.

--- src/lia/indexing/VerboseIndexing.java
+++ src/lia/indexing/VerboseIndexing.java
@@ -19,8 +19,10 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.analysis.WhitespaceAnalyzer;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 
 import java.io.IOException;
 
@@ -30,18 +32,18 @@ public class VerboseIndexing {
   private void index() throws IOException {
 
     Directory dir = new RAMDirectory();
+    IndexWriterConfig wconfig = new IndexWriterConfig(new WhitespaceAnalyzer());
+    wconfig.setInfoStream(System.out);
+   
+    IndexWriter writer = new IndexWriter(dir, wconfig);
 
-    IndexWriter writer = new IndexWriter(dir, new WhitespaceAnalyzer(),
-                                         IndexWriter.MaxFieldLength.UNLIMITED);
-
-    writer.setInfoStream(System.out);
+   
 
     for (int i = 0; i < 100; i++) {
       Document doc = new Document();
-      doc.add(new Field("keyword", "goober", Field.Store.YES, Field.Index.NOT_ANALYZED));
+      doc.add(new StringField("keyword", "goober", Field.Store.YES));
       writer.addDocument(doc);
     }
-    writer.optimize();
     writer.close();
   }
 
